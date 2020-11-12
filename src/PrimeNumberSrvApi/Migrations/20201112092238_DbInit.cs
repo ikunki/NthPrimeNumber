20bbe0a42ff9f86b04b3f38f1bd32e576b72848c
Kun Istvan <ikunki@outlook.com>
2020-11-12 09:24:44 +0000

    ef migrations & update sqlite3-DB

---
+++ src/PrimeNumberSrvApi/Migrations/20201112092238_DbInit.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrimeNumberSrvApi.Migrations
+{
+    public partial class DbInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PrimeNumber",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Index = table.Column<long>(nullable: false),
+                    PrimeValue = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PrimeNumber", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PrimeNumber");
+        }
+    }
+}
